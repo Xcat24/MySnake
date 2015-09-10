7a46c16ed6722fdf60b01a4117ac2a160062a57c
Xcat24 <1131551152@qq.com>
2015-09-10 21:08:13 +0800

    init_git

---
+++ Main.py
@@ -0,0 +1,32 @@
+#encoding:utf8
+class Snake:
+    def __init__(self):
+        #初始化长度为4的蛇
+        #初始化位置（随机）
+    def move(self):
+        #向左右转弯
+        #前进
+    def eat(self):
+        #吃进一个豆子
+        #自身变长
+    def die(self):
+        #判断是否撞到边界
+        #判断是否吃到自己
+        #游戏结束
+    def isfull(self):
+        #判断是否蛇已经满屏
+class Pea:
+    def appear(self):
+        #随机产生一个豆子
+
+class Screen:
+    def __init__(self):
+        #生成游戏界面，15*15
+    def fresh(self):
+        #刷新画面
+
+class Score:
+    def __init__(self):
+        #初始得分
+    def getscore(self):
+        #获取得分
---
+++ test.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    cout<<"＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊"<<endl;
+    cout<<"＊¢¢¢¢¢¢　　　　　　　　　　　　　　＊"<<endl;
+    cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+    cout<<"＊　　　　　　　　　　　　　　　　　　　　＊            低議蛍方�� "<<endl;
+    cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+    cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊　　　　　　　　　　　　　　　　　　　　＊"<<endl;
+	cout<<"＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊"<<endl;
+}
